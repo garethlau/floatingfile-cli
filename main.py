c4462b9146019f8d40c53a217c4b0f804f3363df
Gareth Lau <gareth.kh.lau@gmail.com>
2021-12-23 15:56:51 -0700

    Remove exposed storage methods

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import fire
-from cli.main import (
+from src.main import (
     create_space,
     destroy_space,
     list_files,
@@ -7,15 +7,6 @@ from cli.main import (
     download_files,
     upload_files,
 )
-from cli.storage import (
-    del_code,
-    get_codes,
-    save_code,
-    del_code,
-    resolve_code,
-    set_default,
-)
-
 
 if __name__ == "__main__":
     fire.Fire(
@@ -26,11 +17,5 @@ if __name__ == "__main__":
             "remove": remove_files,
             "download": download_files,
             "upload": upload_files,
-            "codes": get_codes,
-            "save": save_code,
-            "delete": del_code,
-            "resolve": resolve_code,
-            "spaces": get_codes,
-            "set": set_default,
         }
     )
