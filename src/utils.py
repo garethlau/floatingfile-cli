c3e898e108c395ad47e9b4151d684f6bf8d6e0f9
Gareth Lau <gareth.kh.lau@gmail.com>
2021-12-23 17:27:09 -0700

    Add function to get user input for 1 or many indexes

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from requests.api import head
 from .errors import SpaceNotFoundError
 from .config import API_URL, API_KEY
@@ -26,3 +27,27 @@ def get_files(code):
     space = get_space(code)
     files = space["files"]
     return files
+
+
+def index_input(min=0, max=None):
+    user_input = input()
+    user_input_arr = re.split("(?:\s|,)", user_input)
+
+    indexes = set()
+    for x in user_input_arr:
+        # Skip empty string
+        if x.strip() == "":
+            continue
+
+        # Convert to integer
+        index = int(x)
+
+        # Check that the input index is within bounds
+        if index < min:
+            raise ValueError
+        elif max is not None and index > max:
+            raise ValueError
+
+        indexes.add(index)
+
+    return list(indexes)
