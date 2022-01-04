081f7a5081f0b8d00a9eaa259b39d869253e3f99
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-01-04 15:26:20 -0700

    Add clean and compress steps to bundle script

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,1 +1,18 @@
+#! /bin/bash
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+version=$1 
+
+# Cleanup existing builds and artifacts
+rm -rf ./dist
+rm -rf ./build
+
+# Build
 pyinstaller --name floatingfile --add-data '.env:.' --paths env/lib/python3.10/site-packages main.py
+
+# Compress
+tar -C ./dist -czf "floatingfile-$version.tar.gz" floatingfile
