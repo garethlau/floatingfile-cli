9572ce15f6621f6322af0b7eb8950edd4574ebb2
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-01-04 20:50:23 -0700

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -L https://github.com/garethlau/floatingfile-cli/releases/download/v1.1.3/floatingfile-v1.1.3.tar.gz | tar -xz
+curl -L https://github.com/garethlau/floatingfile-cli/releases/download/v1.2.0/floatingfile-v1.2.0.tar.gz | tar -xz
 sudo mv floatingfile /opt/
 sudo ln -s /opt/floatingfile/floatingfile /usr/local/bin
